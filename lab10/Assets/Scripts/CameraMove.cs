d21d1ff9d9e4d2b9cd26ef3d234d0bb60db0d086
Julia <juliyanevar@gmail.com>
2021-12-03 10:30:58 +0300

    lab10

---
+++ lab10/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CameraMove : MonoBehaviour
+{
+    float mouseSensitivity = 100.0f;
+    float clampAngle = 80.0f;
+
+    float rotationY = 0.0f;
+    float rotationX = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 rotation = transform.localRotation.eulerAngles;
+        rotationY = rotation.y;
+        rotationX = rotation.x;
+           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        rotationY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotationX += mouseY * mouseSensitivity * Time.deltaTime;
+
+        Quaternion localRotation = Quaternion.Euler(rotationX, rotationY, 0.0f);
+        transform.rotation = localRotation;
+    }
+}
